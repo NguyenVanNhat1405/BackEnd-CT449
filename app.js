087ccf42cf1a2380dcb7c34b8f1784c968d974bb
Van Nhat <nhatb2012122@student.ctu.edu.vn>
2023-02-15 18:53:57 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) =>{
     res.json({ message: "welcome to contact book application."});
 });
